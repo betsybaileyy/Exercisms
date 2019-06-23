21241a46b861a863bffed7cea33b6c95d808e649
betsybaileyy <betsy.bailey80@gmail.com>
2019-06-22 18:00:42 -0700

    easy peazy

---
+++ python/two-fer/two_fer.py
@@ -0,0 +1,7 @@
+def two_fer(name):
+    if name is not none:
+        two_fer = "One for {}, one for me".format(name)
+        print(two_fer)
+    else:
+        two_fer_2 = "One for you, one for me"
+        print(two_fer_2)
